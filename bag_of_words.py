018d644fec79767f8a99ed82bc0180f760f64467
Eric Grinstein <ericgrinstein@gmail.com>
2015-06-29 11:41:14 -0300

    Migraçao

--- bag_of_words.py
+++ bag_of_words.py
@@ -3,7 +3,7 @@ import nltk
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import CountVectorizer
 
-
+import sys
 
 def _slugify(text):
 	"Removes html tags,punctuation and puts everything in lowercase"
@@ -14,10 +14,30 @@ def _slugify(text):
 
 def _get_meaningful_words(text):
 	"removes stopwords from a slugged text"
-	words = text.split()
+	#stemmer = nltk.stem.PorterStemmer()
+	#words = nltk.word_tokenize(text)
+	#words = nltk.pos_tag(words)
+	words = text.split()	
+
 	stopwords_set = set(nltk.corpus.stopwords.words("english"))
-	words = [w for w in words if not w in stopwords_set]
-	return( " ".join( words ))
+	filtered = ""
+	#words = [w for w in words if not w in stopwords_set]
+	for word in words:
+		if word not in stopwords_set:
+			#print word
+			try:
+				if word[-1] == 's':
+					filtered += word[0:-1]
+				else:
+					filtered += word
+
+				if word == 'not':
+					filtered += '_'
+				else:
+					filtered += ' '
+			except IndexError:
+				continue
+	return filtered
 
 def _create_features(texts):
 	vectorizer = CountVectorizer(analyzer = "word",   
